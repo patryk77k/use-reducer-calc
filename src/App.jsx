f47f646aeccb665cda2070f2775963b01700a616
Patryk Kardach <patrykkardach@Air-von-Patryk.fritz.box>
2023-11-22 17:29:09 +0100

    add DIGIT_ADD

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import "./styles.css";
+import { useReducer, useState } from "react";
+import DigitButton from "./DigitButton";
+
+export const ACTIONS = {
+  ADD_DIGIT: "add_digit",
+  CHOOSE_OPERATION: "choose_operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete_digit",
+  EVALUATE: "evaluate",
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+  }
+};
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">4444*</div>
-        <div className="current-operand">444</div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button>/</button>
-      <button>1</button>
+      <DigitButton digit="1" dispatch={dispatch} />
       <button>2</button>
       <button>3</button>
       <button>*</button>
